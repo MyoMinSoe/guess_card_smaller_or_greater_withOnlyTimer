f7f3255b9d30e19f2d083b8d613c4c178cfaafc0
MyoMinSoe <myominsoe.min@gmail.com>
2024-03-17 13:23:59 +0630

    add card card_model and card_model_and_list

---
+++ lib/card_model_and_list/card_list.dart
@@ -0,0 +1,20 @@
+import 'package:guess_card_smaller_or_greater/card_model_and_list/card_model.dart';
+import 'package:guess_card_smaller_or_greater/constants/assets.dart';
+
+class CardList {
+  List<CardModel> cardlist = [
+    CardModel(image: Assets.assetsA, number: 1),
+    CardModel(image: Assets.assets2, number: 2),
+    CardModel(image: Assets.assets3, number: 3),
+    CardModel(image: Assets.assets4, number: 4),
+    CardModel(image: Assets.assets5, number: 5),
+    CardModel(image: Assets.assets6, number: 6),
+    CardModel(image: Assets.assets7, number: 7),
+    CardModel(image: Assets.assets8, number: 8),
+    CardModel(image: Assets.assets9, number: 9),
+    CardModel(image: Assets.assets10, number: 10),
+    CardModel(image: Assets.assetsJ, number: 11),
+    CardModel(image: Assets.assetsK, number: 12),
+    CardModel(image: Assets.assetsQ, number: 13),
+  ];
+}
---
+++ lib/card_model_and_list/card_model.dart
@@ -0,0 +1,5 @@
+class CardModel {
+  String image;
+  int number;
+  CardModel({required this.image, required this.number});
+}
